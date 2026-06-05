8ba0cf7ea6e89f27f4d49848011a189398e36521
Patrick de Alcantara Strogueia <patrick.strogueia@tivit.com>
2026-06-05 15:45:25 -0300

    Aula 33

--- tests/test_001_login_successful.py
+++ tests/test_001_login_successful.py
@@ -1,9 +1,15 @@
-from playwright.sync_api import Page, expect
+# from playwright.sync_api import Page, expect
 
 
-def test_001_login_successful(page: Page) -> None:
-    page.goto("https://leogcarvalho.github.io/simulabank/login.html")
-    page.get_by_role("textbox", name="Usuário:").fill("user1")
-    page.get_by_role("textbox", name="Senha:").fill("pass1")
-    page.get_by_role("button", name="Entrar").click()
-    expect(page.get_by_role("heading", name="Bem-vindo ao SimulaBank!")).to_be_visible()
+# def test_001_login_successful(page: Page) -> None:
+#     page.goto("https://leogcarvalho.github.io/simulabank/login.html")
+#     page.get_by_role("textbox", name="Usuário:").fill("user1")
+#     page.get_by_role("textbox", name="Senha:").fill("pass1")
+#     page.get_by_role("button", name="Entrar").click()
+#     expect(page.get_by_role("heading", name="Bem-vindo ao SimulaBank!")).to_be_visible()
+
+def test_001_login_successful_pom(login_page) -> None:
+    login_page.login("user1", "pass1")
+    login_page.assert_login_successful()
+
+# pytest --headed --slowmo 1000 -k test_001_login_successful_pom
