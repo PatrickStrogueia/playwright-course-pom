c72f1cc7453b19b8e0f42b64222ede1167a4897d
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-12 16:39:14 -0400

    Aula 43

---
+++ tests/test_004_verificar_emprestimo_contratado.py
@@ -0,0 +1,14 @@
+def test_004_verificar_emprestimo_contratado(common_page, emprestimos_page, home_page, login_page) -> None:
+    login_page.login("user1", "pass1")
+    home_page.acessar_menu("Empréstimos")
+    emprestimos_page.selecionar_valor_emprestimo("5.000,00")
+    emprestimos_page.clicar_contratar_emprestimo()
+    common_page.assert_text("Transação Realizada com Sucesso!")
+    common_page.assert_text("A transação foi concluída com sucesso. Você pode voltar para a página principal e continuar suas operações.")
+    common_page.voltar_home()
+    common_page.assert_text("R$ 10.000,00")
+    home_page.acessar_menu("Empréstimos")
+    common_page.assert_text("Você já tem um empréstimo contratado em andamento. Novos empréstimos não estão disponíveis.")
+    # common_page.page_pause()
+
+# pytest --headed --slowmo 1000 -k test_004_verificar_emprestimo_contratado
