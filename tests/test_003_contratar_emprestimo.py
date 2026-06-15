d5a659f680d798184e20b4d76dacf553cc01a377
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-15 17:03:56 -0400

    Aula 44

--- tests/test_003_contratar_emprestimo.py
+++ tests/test_003_contratar_emprestimo.py
@@ -1,8 +1,9 @@
 def test_003_contratar_emprestimo(common_page, emprestimos_page, home_page, login_page) -> None:
     login_page.login("user1", "pass1")
     home_page.acessar_menu("Empréstimos")
-    emprestimos_page.selecionar_valor_emprestimo("2.000,00")
-    emprestimos_page.clicar_contratar_emprestimo()
+    # emprestimos_page.selecionar_valor_emprestimo("2.000,00")
+    # emprestimos_page.clicar_contratar_emprestimo()
+    emprestimos_page.contratar_emprestimo("2.000,00")
     common_page.assert_text("Transação Realizada com Sucesso!")
     common_page.assert_text("A transação foi concluída com sucesso. Você pode voltar para a página principal e continuar suas operações.")
     common_page.voltar_home()
@@ -12,3 +13,4 @@ def test_003_contratar_emprestimo(common_page, emprestimos_page, home_page, logi
     # common_page.page_pause()
 
 # pytest --headed --slowmo 1000 -k test_003_contratar_emprestimo
+# pytest
